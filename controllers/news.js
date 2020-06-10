1edbf0aff274d9fca002b439472db38600e71f35
Oleg V. Anisov <anisov@obninsk.ru>
2020-06-10 22:43:13 +0300

    added return all news in post

--- controllers/news.js
+++ controllers/news.js
@@ -8,23 +8,18 @@ const get = (req, res) => {
 };
 const post = (req, res) => {
   const { text, title } = req.body;
-  // const news = new News({ title, text });
+
   News.create({ title, text }, (err, doc) => {
     if (err) {
       console.log(err);
       res.sendStatus(400);
     }
     console.log('Сохранен объект:', doc);
-    res.json(doc);
+    News.find({}, (err, doc) => {
+      if (err) return console.log(err);
+      res.json(doc);
+    });
   });
-  // .then((doc) => {
-  //   console.log('Сохранен объект:', doc);
-  //   res.json(doc);
-  // })
-  // .catch((err) => {
-  //   console.log(err);
-  //   res.sendStatus(400);
-  // });
 };
 const patch = (req, res) => {
   const { text, title } = req.body;
