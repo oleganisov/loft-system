05c48e7ee0ec5df5d6e18d05281316168c11d966
Oleg V. Anisov <anisov@obninsk.ru>
2020-06-12 22:44:42 +0300

    add header

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', require(path.join(__dirname, 'api')));
 
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   throw new ErrorHandler(404, 'Page not found');
