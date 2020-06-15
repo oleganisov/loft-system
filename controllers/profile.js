b186ecc04ffa537f4fa8cd2026c2daf9d96adc2a
Oleg V. Anisov <anisov@obninsk.ru>
2020-06-15 11:40:42 +0300

    get profile done

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,17 +1,35 @@
 const Users = require('../models/schemas/users');
 const { ErrorHandler } = require('../helpers/error');
+const { getUserIdFromToken } = require('../auth/token');
 
-const get = (req, res, next) => {
-  Users.find({}, (err, doc) => {
-    if (err) return next(new ErrorHandler(500, err.message));
-    res.json(doc);
-  });
+const get = async (req, res, next) => {
+  const token = req.headers.authorization;
+  const userId = await getUserIdFromToken(token.replace('Bearer ', ''));
+  console.log(userId);
+  Users.findById(
+    userId,
+    'surName firstName middleName username image permission',
+    (err, doc) => {
+      if (err) return next(new ErrorHandler(500, err.message));
+      res.json(doc);
+    }
+  );
 };
 
-const patch = (req, res) => {
+const patch = async (req, res, next) => {
   const { firstName, middleName, surName, oldPassword, newPassword } = req.body;
-  console.log(firstName, middleName, surName, oldPassword, newPassword);
-  res.sendStatus(200);
+  const token = req.headers.authorization;
+  const userId = await getUserIdFromToken(token.replace('Bearer ', ''));
+  console.log(firstName, middleName, surName, oldPassword, newPassword, userId);
+  Users.findByIdAndUpdate(
+    userId,
+    { $set: { firstName, middleName, surName } },
+    { new: true },
+    (err, doc) => {
+      if (err) return next(new ErrorHandler(500, err.message));
+      res.json(doc);
+    }
+  );
 };
 
 module.exports = { get, patch };
