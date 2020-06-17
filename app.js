271699b132d362fa7b799ca5023cdae61b620c9b
Oleg V. Anisov <anisov@jet.su>
2020-06-17 13:11:04 +0300

    change order in app.js

--- app.js
+++ app.js
@@ -4,22 +4,21 @@ const { ErrorHandler, handleError } = require('./helpers/error');
 
 const app = express();
 require('./models/db');
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
+require('./auth/config-passport');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', require(path.join(__dirname, 'api')));
-
-app.use((_, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-require('./auth/config-passport');
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
