9428a3ad399c8be8067eb52551f93b39f8a491e5
Oleg V. Anisov <anisov@jet.su>
2020-06-22 11:56:18 +0300

    add build for heroku

---
+++ build/precache-manifest.b1c995669ccb389cc99836587c0ad294.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fad3165dd201398e06005f79f923d48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78da502e9cd37f5c3776",
+    "url": "/static/js/2.c0360a81.chunk.js"
+  },
+  {
+    "revision": "1c89ff924eb5b1b90b15",
+    "url": "/static/js/main.f572aceb.chunk.js"
+  },
+  {
+    "revision": "74457c4263b95fcfa591",
+    "url": "/static/js/runtime~main.cbd39ad7.js"
+  }
+]);
